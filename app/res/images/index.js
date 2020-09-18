4b75bb8fb656788e4bcace99b4a4d893c04e725a
Shubham Gupta <shubham.gupta@mobicules.com>
2020-09-18 20:29:43 +0530

    first commit

---
+++ app/res/images/index.js
@@ -0,0 +1,17 @@
+const IMAGES ={
+        ARROW:require('./arrow/arrow.png'),
+        ARROW_LEFT_UNSELECTED:require('./arrow_left_unselected/arrow_left_unselected.png'),
+        ARROW_LEFT_SELECTED:require('./arrow_left_selected/arrow_left_selected.png'),
+        ARROW_RIGHT_UNSELECTED:require('./arrow_right_unselected/arrow_right_unselected.png'),
+        ARROW_RIGHT_SELECTED:require('./arrow_right_selected/arrow_right_selected.png'),
+        CALENDAR_RIGHT:require('./calendar_right/calendar_righ.png'),
+        CALENDAR_CROSS:require('./calender_cross/calender_cross.png'),
+        JOIN:require('./join/join.png'),
+        JOIN_UNSELECTED:require('./join_unselected/join_unselected.png'),
+        ONLINE:require('./online/online.png'),
+        VIRTUAL_SELECTED:require('./virtual_selected/virtual_selected.png'),
+        VIRTUAL_UNSELECTED:require('./virtual_unselected/virtual_unselected.png'),
+        WRITE:require('./write/write.png')
+}
+
+export default IMAGES;
